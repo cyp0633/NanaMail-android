d35a1922cd11a07f64bce972b3c634a06b6f4e04
cyp0633 <cyp0633@gmail.com>
2022-11-09 14:52:54 +0800

    Improved experience when setting up the app

--- app/src/main/java/com/hnu/nanamail/ui/screen/Setup.kt
+++ app/src/main/java/com/hnu/nanamail/ui/screen/Setup.kt
@@ -35,6 +35,9 @@ fun SetupScreen(
     val sendEncryptMethod = remember { mutableStateOf("") }
     val sendPortNumber = remember { mutableStateOf("") }
 
+    val modifiedPop3Server = remember { mutableStateOf(false) }
+    val modifiedSmtpServer = remember { mutableStateOf(false) }
+
     // 提示框内容
     var showDialog = false
     var onDismissRequest = {}
@@ -49,33 +52,50 @@ fun SetupScreen(
         )
     }
 
-    Scaffold { PaddingValues ->
+    Scaffold(
+        modifier = Modifier.imePadding()
+    ) { PaddingValues ->
         Column(
             modifier = Modifier
                 .padding(PaddingValues)
                 .padding(horizontal = 40.dp)
                 .verticalScroll(rememberScrollState())
         ) {
+            // 标题
             Text(
                 text = stringResource(id = R.string.finish_basic_settings),
                 modifier = Modifier.padding(vertical = 30.dp),
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.colorScheme.primary
             )
+            // 基本账户设置
             Text(
                 text = stringResource(id = R.string.basic_account_info),
                 modifier = Modifier.padding(vertical = 5.dp),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.secondary
             )
+            // 邮箱地址
             TextField(
                 value = mailAddress.value,
-                onValueChange = { mailAddress.value = it },
+                onValueChange = {
+                    mailAddress.value = it
+                    if(it.contains("@")){
+                        val server = it.substring(it.indexOf("@") + 1)
+                        if(!modifiedPop3Server.value){
+                            pop3Server.value = "pop3.$server"
+                        }
+                        if(!modifiedSmtpServer.value){
+                            smtpServer.value = "smtp.$server"
+                        }
+                    }
+                },
                 label = { Text(text = stringResource(id = R.string.mail_address)) },
                 modifier = Modifier
                     .padding(vertical = 10.dp)
                     .fillMaxWidth(),
             )
+            // 密码
             TextField(
                 value = password.value,
                 onValueChange = { password.value = it },
@@ -86,20 +106,26 @@ fun SetupScreen(
                 visualTransformation = PasswordVisualTransformation()
             )
             Spacer(modifier = Modifier.height(20.dp))
+            // 接收设置
             Text(
                 text = stringResource(id = R.string.receive_server_info),
                 modifier = Modifier.padding(vertical = 5.dp),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.secondary
             )
+            // POP3服务器
             TextField(
                 value = pop3Server.value,
-                onValueChange = { pop3Server.value = it },
+                onValueChange = {
+                    modifiedPop3Server.value = true
+                    pop3Server.value = it
+                },
                 label = { Text(text = stringResource(id = R.string.pop3_server)) },
                 modifier = Modifier
                     .padding(vertical = 10.dp)
                     .fillMaxWidth(),
             )
+            // 加密方式
             TextField(
                 value = receiveEncryptMethod.value,
                 onValueChange = { receiveEncryptMethod.value = it },
@@ -108,6 +134,7 @@ fun SetupScreen(
                     .padding(vertical = 10.dp)
                     .fillMaxWidth(),
             )
+            // 端口号
             TextField(
                 value = receivePortNumber.value,
                 onValueChange = { receivePortNumber.value = it },
@@ -117,20 +144,26 @@ fun SetupScreen(
                     .fillMaxWidth(),
             )
             Spacer(modifier = Modifier.height(20.dp))
+            // 发送设置
             Text(
                 text = stringResource(id = R.string.send_server_info),
                 modifier = Modifier.padding(vertical = 5.dp),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.secondary
             )
+            // SMTP服务器
             TextField(
                 value = smtpServer.value,
-                onValueChange = { smtpServer.value = it },
+                onValueChange = {
+                    modifiedSmtpServer.value = true
+                    smtpServer.value = it
+                },
                 label = { Text(text = stringResource(id = R.string.smtp_server)) },
                 modifier = Modifier
                     .padding(vertical = 10.dp)
                     .fillMaxWidth(),
             )
+            // 加密方式
             TextField(
                 value = sendEncryptMethod.value,
                 onValueChange = { sendEncryptMethod.value = it },
@@ -139,6 +172,7 @@ fun SetupScreen(
                     .padding(vertical = 10.dp)
                     .fillMaxWidth(),
             )
+            // 端口号
             TextField(
                 value = sendPortNumber.value,
                 onValueChange = { sendPortNumber.value = it },
@@ -147,6 +181,7 @@ fun SetupScreen(
                     .padding(vertical = 10.dp)
                     .fillMaxWidth(),
             )
+            // 完成
             Button(
                 onClick = {
                     val result = viewModel.verify(
