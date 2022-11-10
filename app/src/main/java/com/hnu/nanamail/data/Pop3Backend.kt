c2a1b854299e010c8668f1686227d8e3d0e9d2bf
cyp0633 <cyp0633@cyp0633.icu>
2022-11-10 10:50:40 +0800

    POP3 credentials verification
    Signed-off-by: cyp0633 <cyp0633@cyp0633.icu>

--- app/src/main/java/com/hnu/nanamail/data/Pop3Backend.kt
+++ app/src/main/java/com/hnu/nanamail/data/Pop3Backend.kt
@@ -1,5 +1,11 @@
 package com.hnu.nanamail.data
 
+import android.util.Log
+import java.util.*
+import javax.mail.AuthenticationFailedException
+import javax.mail.MessagingException
+import javax.mail.Session
+
 class Pop3Backend(
     var mailAddress: String,
     var password: String,
@@ -8,6 +14,37 @@ class Pop3Backend(
     var portNumber: Int
 ) {
     fun verify(): String {
-        return "success"
+        try {
+            val props = Properties()
+            props["mail.pop3.auth"] = "true"
+            when(encryptMethod) {
+                "SSL" -> {
+                    props["mail.pop3.socketFactory.class"] = "javax.net.ssl.SSLSocketFactory"
+                    props["mail.pop3.socketFactory.fallback"] = "false"
+                    props["mail.pop3.socketFactory.port"] = portNumber
+                }
+                "TLS" -> {
+                    props["mail.pop3.starttls.enable"] = "true"
+                    props["mail.pop3.starttls.required"] = "true"
+                }
+            }
+            val session = Session.getInstance(props)
+            val store = session.getStore("pop3")
+            store.connect(server,portNumber,mailAddress,password)
+            store.close()
+            return "success"
+        } catch (e: AuthenticationFailedException) {
+            Log.e("Pop3Backend", "Authentication failed")
+            e.printStackTrace()
+            return "Authentication failed"
+        } catch (e: MessagingException) {
+            Log.e("Pop3Backend", "MessagingException")
+            e.printStackTrace()
+            return "MessagingException"
+        } catch (e: Exception) {
+            Log.e("Pop3Backend", "Exception")
+            e.printStackTrace()
+            return "Exception"
+        }
     }
 }
