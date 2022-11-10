f07d74865c1970159f09edc13026e6f3f472489a
cyp0633 <cyp0633@gmail.com>
2022-11-10 20:28:01 +0800

    Move network to IO dispatch

--- app/src/main/java/com/hnu/nanamail/viewmodel/SetupViewModel.kt
+++ app/src/main/java/com/hnu/nanamail/viewmodel/SetupViewModel.kt
@@ -4,10 +4,13 @@ import android.app.Application
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hnu.nanamail.dao.AppDatabase
 import com.hnu.nanamail.data.Pop3Backend
 import com.hnu.nanamail.data.SmtpBackend
 import com.hnu.nanamail.data.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SetupViewModel(application: Application) : AndroidViewModel(application) {
     // 输入框内容
@@ -29,7 +32,7 @@ class SetupViewModel(application: Application) : AndroidViewModel(application) {
     var dialogText = ""
 
     fun verify(): String {
-        var result: String
+        var result = ""
         if (mailAddress.value == "" || password.value == "" || pop3Server.value == "" || receiveEncryptMethod.value == "" || receivePortNumber.value == "" || smtpServer.value == "" || sendEncryptMethod.value == "" || sendPortNumber.value == "") {
             return "请填写完整信息"
         }
@@ -40,7 +43,9 @@ class SetupViewModel(application: Application) : AndroidViewModel(application) {
             sendEncryptMethod.value,
             sendPortNumber.value.toInt()
         )
-        result = smtpBackend.verify()
+        viewModelScope.launch(Dispatchers.IO) {
+            result = smtpBackend.verify()
+        }
         if (result != "success") {
             return "SMTP 验证错误：$result"
         }
@@ -51,14 +56,16 @@ class SetupViewModel(application: Application) : AndroidViewModel(application) {
             receiveEncryptMethod.value,
             receivePortNumber.value.toInt()
         )
-        result = pop3Backend.verify()
-        if(result!="success"){
+        viewModelScope.launch(Dispatchers.IO) {
+            result = pop3Backend.verify()
+        }
+        if (result != "success") {
             return "POP3 验证错误：$result"
         }
         // 保存用户信息，目前只有一个用户，所以把以前的先删掉
         val db = AppDatabase.getDatabase(getApplication())
         val existUser = db.userDao().getUser()
-        if(existUser!=null){
+        if (existUser != null) {
             db.userDao().deleteUser(existUser)
         }
         db.userDao().insertUser(
