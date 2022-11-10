9c2e504b917feedcbd65a9fd046e58f2d7840831
cyp0633 <cyp0633@gmail.com>
2022-11-10 15:04:46 +0800

    Trash mail component

--- app/src/main/java/com/hnu/nanamail/ui/screen/Inbox.kt
+++ app/src/main/java/com/hnu/nanamail/ui/screen/Inbox.kt
@@ -1,17 +1,21 @@
 package com.hnu.nanamail.ui.screen
 
 import android.app.Application
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.hnu.nanamail.ui.component.TopBarComponent
 import com.hnu.nanamail.viewmodel.InboxViewModel
+import com.hnu.nanamail.R
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,9 +31,49 @@ fun InboxScreen(
         },
     ) {
         Column (
-            modifier = Modifier.padding(horizontal = 40.dp).padding(it)
+            modifier = Modifier
+                .padding(horizontal = 40.dp)
+                .padding(it)
                 ) {
 
+        }
+    }
+}
+
+// 垃圾邮件箱按钮
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TrashEntryComponent(
+    modifier: Modifier = Modifier,
+    onClick: ()->Unit,
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.secondary,
+        ),
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.extraLarge,
+        onClick = {onClick()}
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = stringResource(id = R.string.trash_mail),
+                modifier = Modifier,
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.unsubscribe),
+                contentDescription = null,
+                modifier = Modifier.size(30.dp)
+            )
         }
     }
 }
@@ -40,6 +84,14 @@ fun InboxScreenPreview() {
     InboxScreen(
         viewModel = InboxViewModel(Application()),
         navController = NavController(Application())
+    )
+}
+
+@Preview
+@Composable
+fun TrashEntryComponentPreview() {
+    TrashEntryComponent(
+        onClick = {}
     )
 }
 
--- app/src/main/java/com/hnu/nanamail/ui/screen/Setup.kt
+++ app/src/main/java/com/hnu/nanamail/ui/screen/Setup.kt
@@ -7,15 +7,12 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.hnu.nanamail.R
 import com.hnu.nanamail.viewmodel.SetupViewModel
