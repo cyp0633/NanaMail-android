a112261b41272c9bc87af7a78fc0ea9bd5ae30e8
cyp0633 <cyp0633@gmail.com>
2022-11-12 15:02:05 +0800

    Outbox and its complete viewmoel

--- app/src/main/java/com/hnu/nanamail/ui/screen/Outbox.kt
+++ app/src/main/java/com/hnu/nanamail/ui/screen/Outbox.kt
@@ -1,13 +1,104 @@
 package com.hnu.nanamail.ui.screen
 
-import androidx.compose.runtime.Composable
+import android.app.Application
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.hnu.nanamail.R
+import com.hnu.nanamail.ui.component.DrawerComponent
+import com.hnu.nanamail.ui.component.MailItemComponent
+import com.hnu.nanamail.ui.component.MainTopBarComponent
 import com.hnu.nanamail.viewmodel.OutboxViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OutboxScreen(
     navController: NavHostController,
     viewModel: OutboxViewModel
 ) {
+    val login = remember {
+        mutableStateOf(true)
+    }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+//    if(!viewModel.checkLogin()) {
+//        login.value = false
+//    }
+    if (!login.value) {
+        AlertDialog(
+            onDismissRequest = {
+                navController.navigate(NavItem.Setup.route)
+            },
+            title = {
+                Text(text = stringResource(id = R.string.no_login))
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    navController.navigate(NavItem.Setup.route)
+//                    login.value = true
+                }) {
+                    Text(text = stringResource(id = R.string.confirm))
+                }
+            }
+        )
+    } else {
+//        viewModel.getMailList()
+    }
+    ModalNavigationDrawer(
+        drawerContent = {
+            DrawerComponent(
+                navController = navController,
+                onClickClose = { scope.launch { drawerState.close() } },
+                selectedItem = NavItem.Outbox
+            )
+        },
+        drawerState = drawerState,
+        gesturesEnabled = false
+    ) {
+        Scaffold(
+            topBar = {
+                MainTopBarComponent(
+                    text = stringResource(id = R.string.outbox),
+                    onClickCompose = {
+                        navController.navigate(NavItem.Compose.route)
+                    },
+                    onClickDrawer = {
+                        scope.launch { drawerState.open() }
+                    }
+                )
+            },
+        ) { PaddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(PaddingValues)
+                    .padding(horizontal = 20.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                for (mail in viewModel.mailList) {
+                    MailItemComponent(mail = mail) {
+                        navController.navigate("mail/${mail.uuid}")
+                    }
+                }
+            }
+        }
+    }
+}
 
+@Preview
+@Composable
+fun OutboxScreenPreview() {
+    OutboxScreen(
+        navController = NavHostController(Application()),
+        viewModel = OutboxViewModel(Application())
+    )
 }
--- app/src/main/java/com/hnu/nanamail/viewmodel/OutboxViewModel.kt
+++ app/src/main/java/com/hnu/nanamail/viewmodel/OutboxViewModel.kt
@@ -1,8 +1,37 @@
 package com.hnu.nanamail.viewmodel
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.hnu.nanamail.dao.AppDatabase
+import com.hnu.nanamail.data.Mail
+import com.hnu.nanamail.data.Pop3Backend
+import com.hnu.nanamail.data.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class OutboxViewModel(application: Application) : AndroidViewModel(application) {
+    var mailList = mutableListOf<Mail>()
+    val page = mutableStateOf(1)
 
+    fun checkLogin(): Boolean {
+        var user: User? = null
+        viewModelScope.launch(Dispatchers.IO) {
+            user = AppDatabase.getDatabase(getApplication()).userDao().getUser()
+        }
+        return user != null
+    }
+
+    fun getMailList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val fetchList = Pop3Backend.fetchInbox()
+            AppDatabase.getDatabase(getApplication()).mailDao()
+                .insertMails(*fetchList.toTypedArray())
+            mailList =
+                AppDatabase.getDatabase(getApplication()).mailDao().getMailListByPage(page.value)
+                    .toMutableStateList()
+        }
+    }
 }
