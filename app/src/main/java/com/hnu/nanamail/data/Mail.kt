f5b85b18c56e27592ee8fad4d1c41ef3a78f3099
cyp0633 <cyp0633@gmail.com>
2022-11-12 17:09:54 +0800

    Update Mail default schema

--- app/src/main/java/com/hnu/nanamail/data/Mail.kt
+++ app/src/main/java/com/hnu/nanamail/data/Mail.kt
@@ -12,49 +12,49 @@ data class Mail(
     // 邮件 UUID
     @PrimaryKey
     @ColumnInfo(name = "uuid")
-    val uuid: String,
+    val uuid: String = "",
     // 所属邮箱账户
     @ColumnInfo(name = "account")
-    val account: String,
+    val account: String = "",
     // 发送方名字，如 cyp0633
     @ColumnInfo(name = "sender")
-    val sender: String,
+    val sender: String = "",
     // 发送邮件地址，如 cyp0633@cyp0633.icu
     @ColumnInfo(name = "sender_address")
-    val senderAddress: String,
+    val senderAddress: String = "",
     // 收件人邮箱，可能有多个
     @ColumnInfo(name = "recipient_to")
-    val recipientTo: String,
+    val recipientTo: String = "",
     // 抄送人邮箱，可能有多个
     @ColumnInfo(name = "recipient_cc")
-    val recipientCc: String,
+    val recipientCc: String = "",
     // 邮件标题
     @ColumnInfo(name = "subject")
-    val subject: String,
+    val subject: String = "",
     // 邮件内容
     @ColumnInfo(name = "content")
-    val content: String,
+    val content: String = "",
     // 预览内容，最多 100 字符
     @ColumnInfo(name = "preview")
-    val preview: String,
+    val preview: String = "",
     // 是否已读
     @ColumnInfo(name = "is_read")
-    val isRead: Boolean,
+    val isRead: Boolean = false,
     // 是否有附件
     @ColumnInfo(name = "has_attachment")
-    val hasAttachment: Boolean,
+    val hasAttachment: Boolean = false,
     // 附件是否已下载
     @ColumnInfo(name = "is_attachment_downloaded")
-    val attachmentDownloaded: Boolean,
+    val attachmentDownloaded: Boolean = false,
     // uid，用于 JavaMail
     @ColumnInfo(name = "uid")
-    val uid: Int,
+    val uid: Int = 0,
     // 类型
     @ColumnInfo(name = "type")
-    val type: MailType,
+    val type: MailType = MailType.INBOX,
     // 时间
     @ColumnInfo(name = "time")
-    val time: Long
+    val time: Long = 0,
 ) {
     fun getTimeStr(): String {
         val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("zh", "CN"))
