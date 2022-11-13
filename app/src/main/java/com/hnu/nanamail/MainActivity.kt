b8be6d30bc9f8422ac171d5ea63a77f619b8e48c
cyp0633 <cyp0633@gmail.com>
2022-11-13 23:57:59 +0800

    Read user info on startup

--- app/src/main/java/com/hnu/nanamail/MainActivity.kt
+++ app/src/main/java/com/hnu/nanamail/MainActivity.kt
@@ -2,24 +2,28 @@ package com.hnu.nanamail
 
 import android.app.Application
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import com.hnu.nanamail.dao.AppDatabase
+import com.hnu.nanamail.data.User
 import com.hnu.nanamail.ui.screen.NavScreen
 import com.hnu.nanamail.ui.theme.NanaMailTheme
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GlobalScope.launch {
+            getUser()
+        }
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             NanaMailTheme {
@@ -31,6 +35,15 @@ class MainActivity : ComponentActivity() {
                     NavScreen(application = application)
                 }
             }
+        }
+    }
+
+    // 从数据库中获取当前用户信息
+    private fun getUser() = CoroutineScope(Dispatchers.IO).launch {
+        val db = AppDatabase.getDatabase(application)
+        val user = db.userDao().getUser()
+        if (user != null) {
+            User.currentUser = user
         }
     }
 }
