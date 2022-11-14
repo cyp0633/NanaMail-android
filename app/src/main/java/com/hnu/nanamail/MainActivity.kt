2453addc275ec43802cae0d88093ee4a8830c9b6
cyp0633 <cyp0633@gmail.com>
2022-11-14 20:59:08 +0800

    Edge to edge view

--- app/src/main/java/com/hnu/nanamail/MainActivity.kt
+++ app/src/main/java/com/hnu/nanamail/MainActivity.kt
@@ -4,13 +4,17 @@ import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.hnu.nanamail.dao.AppDatabase
 import com.hnu.nanamail.data.User
 import com.hnu.nanamail.ui.screen.NavScreen
@@ -21,13 +25,24 @@ class MainActivity : ComponentActivity() {
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 用于沉浸式状态栏
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setDecorFitsSystemWindows(false)
+
         GlobalScope.launch {
             getUser()
         }
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             NanaMailTheme {
-                // A surface container using the 'background' color from the theme
+                // 用于沉浸式状态栏
+                val systemUiController = rememberSystemUiController()
+                val isDark = isSystemInDarkTheme()
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isDark
+                    )
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
