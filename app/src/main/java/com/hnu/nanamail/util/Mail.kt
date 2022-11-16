8ae9ac86e38cae81bcadaeacd2368b7fff7f9b4e
cyp0633 <cyp0633@gmail.com>
2022-11-16 11:16:57 +0800

    Parse text content correctly

--- app/src/main/java/com/hnu/nanamail/util/Mail.kt
+++ app/src/main/java/com/hnu/nanamail/util/Mail.kt
@@ -4,12 +4,12 @@ import android.util.Log
 import com.hnu.nanamail.data.Mail
 import com.hnu.nanamail.data.MailType
 import com.hnu.nanamail.data.Pop3Backend
-import java.util.*
-import javax.mail.Flags
-import javax.mail.Message
-import javax.mail.Multipart
-import javax.mail.Part
+import java.io.IOException
+import javax.mail.*
+import javax.mail.internet.ContentType
 import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMultipart
+
 
 // 是否已读
 fun isRead(message: Message): Boolean {
@@ -38,22 +38,6 @@ fun hasAttachment(message: Message): Boolean {
         e.printStackTrace()
     }
     return false
-}
-
-// 解析邮件内容
-fun parseContent(message: Message): String {
-    val contentType = message.contentType
-    if (contentType.contains("multipart")) {
-        val multipart = message.content as Multipart
-        for (i in 0 until multipart.count) {
-            val bodyPart = multipart.getBodyPart(i)
-            if (Part.ATTACHMENT.equals(bodyPart.disposition, ignoreCase = true)) {
-                continue
-            }
-            return bodyPart.content.toString()
-        }
-    }
-    return message.content.toString()
 }
 
 // 解析收件人
@@ -86,11 +70,20 @@ fun parseMessagesIntoMails(msg: Array<Message>, mailType: MailType): List<Mail> 
     var mailList = mutableListOf<Mail>()
     for (m in msg) {
         val isRead = isRead(m)
-//        val hasAttachment = hasAttachment(m)
-        val hasAttachment = false
-//        val content = parseContent(m)
-        val content = "test"
-        // TODO: 解析图片
+        val hasAttachment = hasAttachment(m)
+        var content = "test"
+        try {
+            content = getTextFromMessage(m)
+        } catch (e: MessagingException) {
+            e.printStackTrace()
+            Log.e("MailUtil", "messaging exception: ${e.message}, ${e.cause}")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("MailUtil", "io exception: ${e.message}, ${e.cause}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("MailUtil", "exception: ${e.message}, ${e.cause}")
+        }
         // 发送者和其邮箱
         val fromAddress = (m.from[0] as InternetAddress).address
         val from = (m.from[0] as InternetAddress).personal
@@ -121,4 +114,75 @@ fun parseMessagesIntoMails(msg: Array<Message>, mailType: MailType): List<Mail> 
         mailList = mailList.plus(mail) as MutableList<Mail>
     }
     return mailList
+}
+
+/**
+ * 获取邮件文本内容
+ *
+ * From [Stack Overflow](https://stackoverflow.com/a/36932127)
+ *
+ * @param message 邮件
+ * @return 邮件文本内容
+ */
+@Throws(IOException::class, MessagingException::class)
+private fun getTextFromMessage(message: Message): String {
+    var result: String = ""
+    if (message.isMimeType("text/plain")) {
+        result = message.content.toString()
+    } else if (message.isMimeType("multipart/*")) {
+        val mimeMultipart = message.content as MimeMultipart
+        result = getTextFromMimeMultipart(mimeMultipart)
+    }
+    return result
+}
+
+/**
+ * 递归获取邮件文本内容
+ *
+ * From [Stack Overflow](https://stackoverflow.com/a/36932127)
+ *
+ * @param mimeMultipart 邮件
+ * @return 邮件文本内容
+ */
+@Throws(IOException::class, MessagingException::class)
+private fun getTextFromMimeMultipart(
+    mimeMultipart: MimeMultipart
+): String {
+    val count = mimeMultipart.count
+    if (count == 0) throw MessagingException("Multipart with no body parts not supported.")
+    val multipartAlt: Boolean =
+        ContentType(mimeMultipart.contentType).match("multipart/alternative")
+    if (multipartAlt) // alternatives appear in an order of increasing
+    // faithfulness to the original content. Customize as req'd.
+        return getTextFromBodyPart(mimeMultipart.getBodyPart(count - 1))
+    var result: String = ""
+    for (i in 0 until count) {
+        val bodyPart = mimeMultipart.getBodyPart(i)
+        result += getTextFromBodyPart(bodyPart)
+    }
+    return result
+}
+
+/**
+ * 递归获取邮件文本内容
+ *
+ * From [Stack Overflow](https://stackoverflow.com/a/36932127)
+ *
+ * @param bodyPart 邮件
+ * @return 邮件文本内容
+ */
+@Throws(IOException::class, MessagingException::class)
+private fun getTextFromBodyPart(
+    bodyPart: BodyPart
+): String {
+    var result = ""
+    if (bodyPart.isMimeType("text/plain")) {
+        result = bodyPart.content as String
+    } else if (bodyPart.isMimeType("text/html")) {
+        val html = bodyPart.content as String
+        result = org.jsoup.Jsoup.parse(html).text()
+    } else if (bodyPart.content is MimeMultipart) {
+        result = getTextFromMimeMultipart(bodyPart.content as MimeMultipart)
+    }
+    return result
 }
